72360ba9af4c7518f8ea57f89e25e39112a6c777
Viper Eyes <Serpent_Eyesus@hotmail.com>
2021-10-23 06:42:35 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from "./Pages/Login/Login";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/login" name="Login" component={Login} />
         <Route exact path="/" name="Home" component={AppContainer} />
