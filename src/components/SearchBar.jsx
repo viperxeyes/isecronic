856cb2d85df8dcbe56922704f35320d6a095e73a
ViperxEyes <serpent_eyesus@hotmail.com>
2022-05-25 04:00:35 +0200

    Scroll bar removed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaSearch } from "react-icons/fa";
 const SearchBar = () => {
   return (
-    <div className="flex items-center w-full  bg-slate-800/60 border-2 border-transparent focus-within:border-blue-500/50 rounded-xl px-2 transition-all duration-300 ease-out shadow-md">
+    <div className="flex items-center min-w-fit w-1/2  bg-slate-800/60 border-2 border-transparent focus-within:border-blue-500/50 rounded-xl px-2 transition-all duration-300 ease-out shadow-md">
       <FaSearch className="text-blue-500" />
       <input
         type={"search"}
